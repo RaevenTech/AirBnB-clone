5450c04582af20237e0ce5e1e2383ad9ddcccd75
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-10-06 11:58:44 +0100

    Creating navbar

---
+++ air-clone/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1> navbar</h1>
+            <img src="../assets/airbnb1.png" alt="logo" />
+        </nav>
+    );
+};
+
+export default Navbar;
